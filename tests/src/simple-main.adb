341c0d290462df7fb99c47a35a5a88281a26fecb
persan <per.s.sandberg@bahnhof.se>
2017-01-20 19:24:23 +0100

    Remove compiler warnings.

--- tests/src/simple-main.adb
+++ tests/src/simple-main.adb
@@ -4,9 +4,9 @@ procedure Simple.Main is
    use Simple.Text_Io;
    use Ada.Text_IO;
 
-   F : Simple_Struct := (12, (others => '1'));
-   F2 : Struct_With_Discriminant_and_Default := (True, 12, (others => '1'), False);
-   F3 : Struct_With_Discriminant_and_Default := (False, 12, (others => '1'), 1);
+   F : constant Simple_Struct := (12, (others => '1'));
+   F2 : constant Struct_With_Discriminant_and_Default := (True, 12, (others => '1'), False);
+   F3 : constant Struct_With_Discriminant_and_Default := (False, 12, (others => '1'), 1);
    F4 : Struct_With_Discriminant_and_Default;
    F5 : Tagged_Record_Childern;
    F6 : Standard_Types_Record;
