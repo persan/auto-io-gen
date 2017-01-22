c5476d5feeb4b70f2531888de5cf43b63f8f650c
persan <per.s.sandberg@bahnhof.se>
2017-01-22 07:25:28 +0100

    Remove all unused files from "SAL"

--- sal/tests/test-config_files-all_suite.ads
+++
@@ -1,2 +0,0 @@
-with AUnit.Test_Suites;
-function Test.Config_Files.All_Suite return AUnit.Test_Suites.Access_Test_Suite;
--- sal/tests/test-config_files-suit_2.ads
+++
@@ -1,3 +0,0 @@
-with AUnit.Test_Suites;
-
-function Test.Config_Files.Suit_2 return AUnit.Test_Suites.Access_Test_Suite;
